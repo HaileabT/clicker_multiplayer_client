449d83fb2c9bcc1ce48210c76cdcf5f3692954e4
HaileabT <Haileabtesfaye11@gmail.com>
2024-09-13 03:45:58 -0400

    initialized the cliker game with boilerplate

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,5 @@
+export default function Signin() {
+    return <div className=" bg-white">
+        Hello, this is the sign in page
+    </div>
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      bg_primary: "#F2F2F2",
+      bg_secondary: "#0D0D0D",
+    },
     extend: {
       colors: {
         background: "var(--background)",
