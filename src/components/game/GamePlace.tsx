0f1ba75361ca12f12143954eb12503415e6b9162
HaileabT <Haileabtesfaye11@gmail.com>
2024-09-15 09:11:32 -0400

    added transition to player boxes increasing and descreasing

--- src/components/game/GamePlace.tsx
+++ src/components/game/GamePlace.tsx
@@ -9,7 +9,7 @@ interface GamePlaceProps {
 
 export default function GamePlace({ LeftUser, RightUser, width } : GamePlaceProps) {
     return (
-      <div className="grid h-full" style={{ gridTemplateColumns: `${width.col1} ${width.col2}` }}>
+      <div className="grid h-full transition-all" style={{ gridTemplateColumns: `${width.col1} ${width.col2}` }}>
         <div onClick={LeftUser} className="bg-bg_secondary"></div>
         <div onClick={RightUser} className="bg-bg_black"></div>
       </div>
