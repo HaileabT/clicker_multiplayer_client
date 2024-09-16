91b27adcfe1c23de73a1d2ace476a3ac70de748f
HaileabT <Haileabtesfaye11@gmail.com>
2024-09-16 10:41:03 -0400

    removed menu from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default async function Home() {
   return (
     <div className="w-[calc(100%-10rem)] flex flex-col gap-[2.5rem] max-w-[100rem] h-full">
       <ContentContainer component={EnterMode} />
-      <Menu />
     </div>
   )
 }
